92933612b20f57f274cbfe0f732ee0e3785bd3c8
uwagboe <sm.uwagboe@gmail.com>
2020-07-10 16:28:33 -0400

    practice push

---
+++ Skin-T.App/supporting files/TabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarController.swift
+//  Skin-T.App
+//
+//  Created by Pursuit on 7/10/20.
+//  Copyright © 2020 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+    
+
+
+}
